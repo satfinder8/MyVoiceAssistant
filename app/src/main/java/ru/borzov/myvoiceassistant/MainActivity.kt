0ec36fed67c6eeaf41f9c662dea9a490f09500a9
ivan.borzov <satfinder8@gmail.com>
2022-06-15 00:10:03 +0500

    First Task

--- app/src/main/java/ru/borzov/myvoiceassistant/MainActivity.kt
+++ app/src/main/java/ru/borzov/myvoiceassistant/MainActivity.kt
@@ -2,10 +2,20 @@ package ru.borzov.myvoiceassistant
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val name: String = "Ivan"
+        val surname: String = "Ivanov"
+        val age: Int = 37
+        val height: Double = 172.2
+
+        var output = findViewById<TextView>(R.id.output)
+        output.text = "name: ${name} surname: ${surname} age: ${age} height: ${height}"
+
     }
 }
